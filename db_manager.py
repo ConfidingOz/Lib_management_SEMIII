791b32a06f2e2b525bf7ffd5e83a09bc7f18f8a4
Aleric Cusher <oz@MacBook-Air.local>
2020-08-19 21:28:21 +0530

    WORKS

--- db_manager.py
+++ db_manager.py
@@ -1,7 +1,8 @@
 import sqlite3
 from sqlite3 import Error
 import datetime
-from settings import lease_days
+from settings import lease_days,date_format
+import time
 
 
 class Database(object):
@@ -10,6 +11,9 @@ class Database(object):
         self.est_conn()
         self.db_check()
         self.c.execute('''PRAGMA foreign_keys = 1''')
+
+    def __del__(self):
+        self.close_conn()
         
     def est_conn(self):
         self.conn = None
@@ -25,15 +29,21 @@ class Database(object):
             self.c.close()
             self.conn.close()
 
+    def convert_to_date(self, unix):
+        return datetime.datetime.fromtimestamp(unix)
+
     def executor(self, query, values=None):
-        if values:
-            if values is tuple:
-                self.c.execute(query, values)
+        try:
+            if values:
+                if type(values) is tuple:
+                    self.c.execute(query, values)
+                else:
+                    print(f'The argument "value" should be a tuple, it is {values}')
             else:
-                print(f'The argument "value" should be a tuple, it is {values}')
-        else:
-            self.c.execute(query)
-        self.conn.commit()
+                self.c.execute(query)
+            self.conn.commit()
+        except Error as e:
+            print(f'That failed, The error was: {e}')
 
     def db_check(self):
         student_table = '''
@@ -59,8 +69,8 @@ class Database(object):
             Student_reg TEXT NOT NULL,
             Book_id INTEGER NOT NULL,
             Book_name TEXT NOT NULL,
-            Borrow_date TIMESTAMP NOT NULL,
-            Return_date TIMESTAMP NOT NULL,
+            Borrow_date INTEGER NOT NULL,
+            Return_date INTEGER NOT NULL,
             Days_to_return INTEGER,
             FOREIGN KEY (Student_reg) REFERENCES Students(Reg_No),
             FOREIGN KEY (Book_id) REFERENCES Books(Book_ID) ON DELETE CASCADE,
@@ -85,7 +95,7 @@ class Database(object):
             query = '''
             DELETE FROM Students WHERE Reg_No=?
             '''
-            self.executor(query, (Reg,))
+            self.c.execute(query, (Reg,))
         except Error as e:
             query = '''
             SELECT Book_name FROM Borrowed_Books WHERE Student_reg = ?
@@ -95,31 +105,30 @@ class Database(object):
             for i in self.c.fetchall():
                 owed.append(i[0])
             print(f"Cannot remove student! The books owed are: {owed}")
-            print(f'The error was: {e}')
 
-    def remove_book(self, Name): ####Cascade delete the borrowed record check###
+    def remove_book(self, ID): ####Cascade delete the borrowed record check###
         try:
             query = '''
-            DELETE FROM Books WHERE Name=?
+            DELETE FROM Books WHERE Book_ID=?
             '''
-            self.executor(query, (Name,))
+            self.c.execute(query, (ID,))
         except Error as e:
             print(e)
 
-    def borrow_book(self, Student_Name, Reg_No, Book_ID, today=datetime.date.today(), days=lease_days):
-        query = 'SELECT Quantity, Name FROM Books where Book_id = ?'
-        self.executor(query, (Book_ID,))
+    def borrow_book(self, Reg_No, Book_ID, today=int(time.time()), days=lease_days):
+        self.executor('SELECT Quantity, Name FROM Books where Book_id = ?', (Book_ID,))
         Total, BName = self.c.fetchall()[0]
-        self.executor('SELECT * FROM Borrowed_Books WHERE Book_id = ?', (Book_ID,))
+        self.executor('SELECT Book_id FROM Borrowed_Books WHERE Book_id = ?', (Book_ID,))
         borrowed = len(self.c.fetchall())
         if Total - borrowed > 0:
-            return_date = today + datetime.timedelta(days=days)
-            values = (Reg_No, Book_Name, today, return_date,)
+            return_date = (self.convert_to_date(today) + datetime.timedelta(days=days)).timestamp()
+            values = (Reg_No, Book_ID, BName, today, return_date,)
+            # print(values)
             query = '''
             INSERT INTO Borrowed_Books(Student_reg, Book_id, Book_name, Borrow_date, Return_date)
-            VALUES(?, ?, ?, ?)
+            VALUES(?,?,?,?,?)
             '''
-            self.executor(query, (values,))
+            self.executor(query, values)
         else:
             print("Book not available")
 
@@ -133,29 +142,55 @@ class Database(object):
 
     def check_book(self, BName):
         query = '''
-        SELECT Name FROM Books WHERE Name LIKE "%"||?||"%"
+        SELECT Book_ID, Name FROM Books WHERE Name LIKE "%"||?||"%"
         '''
         self.executor(query, (BName,))
-        return self.c.fetchall()
+        books = self.c.fetchall()
+        for i in books:
+            print(f'{i[0]}: {i[1]}')
 
     def check_student(self, Name=None, Reg=None):
         query = '''
         SELECT Name, Reg_No FROM Students WHERE Name LIKE "%"||?||"%" OR Reg_No LIKE "%"||?||"%"
         '''
         self.executor(query, (Name, Reg,))
-        return self.c.fetchall()
-        
+        students = self.c.fetchall()
+        for i in students:
+            print(f'{i[1]}: {i[0]}')
 
     def check_leases(self):
-        today = datetime.date.today()
-        query = '''SELECT Student_reg, Book_name, Return_date FROM Borrowed_Books WHERE Return_date >= ?'''
-        self.executor(query, (today))
-        all_books = {}
+        today = int(time.time())
+        query = '''SELECT Student_reg, Book_name, Return_date FROM Borrowed_Books WHERE Return_date > ?'''
+        self.executor(query, (today,))
         data = self.c.fetchall()
+        reg_based = {}
+        book_based = {}
         for i in data:
-            if i[0] not in all_books:
-                all_books[i[0]] = list()
-            all_books[i[0]].append([i[1], i[2]])
+            print(i)
+        # for each in data:
+        #     if each[0] not in reg_based:
+        #         reg_based[each[0]] = []
+        #     reg_based[each[0]].append((each[1], self.convert_to_date(each[2]).strftime(date_format)))
+        #     if each[1] not in book_based:
+        #         book_based[each[1]] = []
+        #     book_based[each[1]].append((each[0], self.convert_to_date(each[2]).strftime(date_format)))
+        # print("Based on register number:")
+        # for key, att in reg_based.items():
+        #     print(f'{key}:')
+        #     for i in att:
+        #         print(f'{i[0]}\t{i[1]}')
+        # print('\nBased on Books:')
+        # for key, att in book_based.items():
+        #     print(f'{key}:')
+        #     for i in att:
+        #         print(f'{i[0]}\t{i[1]}')
+        # print(reg_based)
+        # print(book_based)
+            
+            
+
+
+
         
 
 
--- settings.py
+++ settings.py
@@ -1,2 +1,3 @@
 lease_days = 10
 lease_fine = 50
+date_format = '%d-%m-%Y'
